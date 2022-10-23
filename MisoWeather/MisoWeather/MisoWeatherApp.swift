0bcc1bef247f1ecbdb8e922274c9906fe2afceee
jiinheo <gjwldls123@gmail.com>
2022-10-23 17:38:20 +0900

    initial commit

---
+++ MisoWeather/MisoWeather/MisoWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisoWeatherApp.swift
+//  MisoWeather
+//
+//  Created by jiin heo on 2022/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MisoWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
